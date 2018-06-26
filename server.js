d83df4f7032464f9783bbf77addba235c9bdf912
Rohan Arora <rohanraarora@gmail.com>
2018-06-27 00:26:46 +0530

    Change options for prerender server and add in-memory cache

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
 
-var server = prerender();
+var server = prerender({
+  logRequests: true,
+  pageLoadTimeout: 5 * 60 * 1000,
+  waitAfterLastRequest: 1000
+});
+
 
 server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.blockResources());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
+
+server.use(require('prerender-memory-cache'))
 
 server.start();
