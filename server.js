87a7845e35c3b21007658a3553cef5e91b6bbbd7
Rohan Arora <rohanraarora@gmail.com>
2018-06-27 00:43:34 +0530

    Add in-memory cache and remove waitAfterLastRequest and add it to the env

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ var prerender = require('./lib');
 
 var server = prerender({
   logRequests: true,
-  pageLoadTimeout: 5 * 60 * 1000,
-  waitAfterLastRequest: 5 * 1000
+  pageLoadTimeout: 5 * 60 * 1000
 });
 
 
@@ -13,6 +12,6 @@ server.use(prerender.sendPrerenderHeader());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 
-// server.use(require('prerender-memory-cache'))
+server.use(require('prerender-memory-cache'))
 
 server.start();
