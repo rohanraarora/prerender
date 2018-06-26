2b20a773e521237a4e5a002b1d7afac66f6d51e0
Rohan Arora <rohanraarora@gmail.com>
2018-06-27 00:36:37 +0530

    Allow script tags in rendered page

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var server = prerender({
 
 server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.blockResources());
-server.use(prerender.removeScriptTags());
+// server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 
 server.use(require('prerender-memory-cache'))
