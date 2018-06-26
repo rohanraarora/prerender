3dcb38bb21f454855e326cdd031bd0c541cdff1c
Rohan Arora <rohanraarora@gmail.com>
2018-06-27 00:40:48 +0530

    Chnage waitAfterLastRequest option

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var prerender = require('./lib');
 var server = prerender({
   logRequests: true,
   pageLoadTimeout: 5 * 60 * 1000,
-  waitAfterLastRequest: 1000
+  waitAfterLastRequest: 5 * 1000
 });
 
 
@@ -13,6 +13,6 @@ server.use(prerender.sendPrerenderHeader());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 
-server.use(require('prerender-memory-cache'))
+// server.use(require('prerender-memory-cache'))
 
 server.start();
